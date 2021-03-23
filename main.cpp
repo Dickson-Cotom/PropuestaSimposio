824cb6fbd939e600b04b26c12a8e515e88d9d908
Dickson-Cotom <78808712+Dickson-Cotom@users.noreply.github.com>
2021-03-23 10:17:25 -0600

    Mi Programa Parcial1
    Parcial

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
+//Dickson Everardo Cotom De León - 1669921
 #include <iostream>
 int main()
 {
   std::cout << "Propuesta de programa para el simposio de Ingenieria 2021\n";
   std::cin.ignore();
+  git.ingnore(main.obj)
+
   return 0;
 }
